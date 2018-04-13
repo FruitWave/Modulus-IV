d6e917002bb6f8551484885b65f540c9b211aade
FruitWave <league@WorthyMini-5.attlocal.net>
2018-04-12 19:16:00 -0700

    End of Module 4

--- src/threads/OlympicRings_Threaded.java
+++ src/threads/OlympicRings_Threaded.java
@@ -1,7 +1,46 @@
 package threads;
 
+import org.jointheleague.graphical.robot.Robot;
+
 public class OlympicRings_Threaded {
-	// Make A Program that uses Threads and robots to draw the Olympic rings. One robot should draw one ring simultaneously with the other 4 robots.
-
+	// Make A Program that uses Threads and robots to draw the Olympic rings. One
+	// robot should draw one ring simultaneously with the other 4 robots.
+	public static void main(String[] args) {
+		Robot ae = new Robot(1000, 400);
+		Robot bea = new Robot(1000, 500);
+		Robot sie = new Robot(500, 500);
+		Robot dei = new Robot(500, 400);
+		new Thread(() -> {
+			ae.setSpeed(100000000);
+			ae.penDown();
+			for (int i = 0; i < 360; i++) {
+				ae.move(5);
+				ae.turn(1);
+			}
+		}).start();
+		new Thread(() -> {
+			bea.setSpeed(100000000);
+			bea.penDown();
+			for (int i = 0; i < 360; i++) {
+				bea.move(5);
+				bea.turn(1);
+			}
+		}).start();
+		new Thread(() -> {
+			sie.setSpeed(100000000);
+			sie.penDown();
+			for (int i = 0; i < 360; i++) {
+				sie.move(5);
+				sie.turn(1);
+			}
+		}).start();
+		new Thread(() -> {
+			dei.setSpeed(100000000);
+			dei.penDown();
+			for (int i = 0; i < 360; i++) {
+				dei.move(5);
+				dei.turn(1);
+			}
+		}).start();
+	}
 }
-
